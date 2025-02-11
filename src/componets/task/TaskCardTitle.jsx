980f3f8fc5224a62b28cadc2e41c13b94dcfacb0
daikigotou <daiki.7634@gmail.com>
2025-02-12 03:07:38 +0900

    カレンダー形式で日付入力できるように修正

--- src/componets/task/TaskCardTitle.jsx
+++ src/componets/task/TaskCardTitle.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Datetime from "react-datetime";
 
 const TaskCardTitle = () => {
   const [isClick, setIsClick] = useState(false);
@@ -21,25 +22,36 @@ const TaskCardTitle = () => {
     setIsClick(false);
   };
 
+  const weekendsInvalidDate = (current) => {
+    return current.day() !== 0 && current.day() !== 6;
+  };
+
   return (
     <div onClick={handleClick} className="taskCardTitleInputArea">
-      {isClick ? (
-        <form onSubmit={handleSubmit}>
-          <input
-            className="taskCardTitleInput"
-            autoFocus
-            type="text"
-            onChange={hadleChange}
-            onBlur={handleBlur}
-            value={inputCardTitle}
-            maxLength={10}
-          />
-        </form>
-      ) : (
-        <h3>{inputCardTitle}</h3>
-      )}
+      <Datetime
+        initialValue={new Date()}
+        dateFormat={"YYYY年MM月DD日"}
+        timeFormat={false}
+        isValidDate={weekendsInvalidDate}
+      />
     </div>
   );
 };
 
 export default TaskCardTitle;
+
+// {isClick ? (
+//   <form onSubmit={handleSubmit}>
+//     <input
+//       className="taskCardTitleInput"
+//       autoFocus
+//       type="text"
+//       onChange={hadleChange}
+//       onBlur={handleBlur}
+//       value={inputCardTitle}
+//       maxLength={10}
+//     />
+//   </form>
+// ) : (
+//   <h3>{inputCardTitle}</h3>
+// )}
